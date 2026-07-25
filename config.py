3e8ac32fdb9446ba78cac78735c44578d766a922
wnxlss <sigma1sigmavich1@gmail.com>
2026-07-25 21:57:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = "8667497753:AAHwKYf4geuBcAMCadMjRbBGjYabGaF5jAk"
-ADMINS = [7910618692]
-crypto = "514407:AAYmqI3VxJYnDiKplzJSS3ZX1wOWDsIhtxD"
+TOKEN = "8792731203:AAE0JE9NPoDzUB7NqMqv64Ye_gY0BoVFUKQ"
+ADMINS = [8515228966]
+crypto = "8792731203:AAE0JE9NPoDzUB7NqMqv64Ye_gY0BoVFUKQ"
 GROQ_API_KEY = "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"
 
 API_ID = 24986768
@@ -14,13 +14,13 @@ API = [
 
 MAX_MIRRORS_PER_USER = 5
 MIRROR_CLEANUP_DAYS = 360
-MAIN_BOT_USERNAME = "triadaSNOS_bot"
+MAIN_BOT_USERNAME = "tgvsgbs_bot"
 
-bot_logs = -1003774166432
+bot_logs = -1004474485806
 
-idd = 7910618692 # поддержки айди
+idd = 7893221881 # поддержки айди
 
-bot_username = "triadaSNOS_bot" # referalka  
+bot_username = "tgvsgbs_bot" # referalka  
 
 bot_channel_link = "https://t.me/wbank_ru"
 bot_channel_id = -1003572156104
@@ -31,10 +31,10 @@ bot_channel_link2 = "https://t.me/snos_triada"
 bot_channel_id2 = -1003445656607
 
 
-subscribe_1_day_usd = 0.50
-subscribe_7_days_usd = 2
-subscribe_30_days_usd = 5
-subscribe_infinity_days_usd = 8
+subscribe_1_day_usd = 0.26
+subscribe_7_days_usd = 1.5
+subscribe_30_days_usd = 4
+subscribe_infinity_days_usd = 5
 subscribe_premium = 10
 
 referral_reward_days = 3
